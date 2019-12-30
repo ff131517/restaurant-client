d156afe2619f15c28f1ea5cda59a4b4a69624683
michaelFang <fangtongjs@gmail.com>
2019-12-30 23:40:28 +0800

    feat:add from

--- src/app/pages/food/detail/food-detail.component.ts
+++ src/app/pages/food/detail/food-detail.component.ts
@@ -12,9 +12,7 @@ import { FoodType } from "../food.common";
 export class FoodDetailComponent implements OnInit {
   validateForm: FormGroup;
   listOfOption: Array<{ label: string; value: number }> = [];
-
   loading = false;
-  avatarUrl: string;
   constructor(private fb: FormBuilder, private msg: NzMessageService) {}
   ngOnInit(): void {
     this.listOfOption = [{ label: "东北菜", value: FoodType.Dongbei }, { label: "粤菜", value: FoodType.Guangdong }, { label: "川菜", value: FoodType.Sichuan }];
@@ -22,81 +20,19 @@ export class FoodDetailComponent implements OnInit {
       name: [null, [Validators.required]],
       desc: [null, [Validators.required]],
       price: [null, [Validators.required]],
-      type: [null, [Validators.required]]
+      type: [null, [Validators.required]],
+      imgUrl: [null, [Validators.required]]
     });
   }
 
-  beforeUpload = (file: File) => {
-    return new Observable((observer: Observer<boolean>) => {
-      const isJPG = file.type === "image/jpeg";
-      if (!isJPG) {
-        this.msg.error("You can only upload JPG file!");
-        observer.complete();
-        return;
-      }
-      const isLt2M = file.size / 1024 / 1024 < 2;
-      if (!isLt2M) {
-        this.msg.error("Image must smaller than 2MB!");
-        observer.complete();
-        return;
-      }
-      // check height
-      this.checkImageDimension(file).then(dimensionRes => {
-        if (!dimensionRes) {
-          this.msg.error("Image only 300x300 above");
-          observer.complete();
-          return;
-        }
 
-        observer.next(isJPG && isLt2M && dimensionRes);
-        observer.complete();
-      });
-    });
-  };
-
-  private getBase64(img: File, callback: (img: string) => void): void {
-    const reader = new FileReader();
-    reader.addEventListener("load", () => callback(reader.result!.toString()));
-    reader.readAsDataURL(img);
-  }
-
-  private checkImageDimension(file: File): Promise<boolean> {
-    return new Promise(resolve => {
-      const img = new Image(); // create image
-      img.src = window.URL.createObjectURL(file);
-      img.onload = () => {
-        const width = img.naturalWidth;
-        const height = img.naturalHeight;
-        window.URL.revokeObjectURL(img.src!);
-        resolve(width === height && width >= 300);
-      };
-    });
-  }
-
-  handleChange(info: { file: UploadFile }): void {
-    switch (info.file.status) {
-      case "uploading":
-        this.loading = true;
-        break;
-      case "done":
-        // Get this url from response in real world.
-        this.getBase64(info.file!.originFileObj!, (img: string) => {
-          this.loading = false;
-          this.avatarUrl = img;
-        });
-        break;
-      case "error":
-        this.msg.error("Network error");
-        this.loading = false;
-        break;
-    }
-  }
 
   submitForm(): void {
     for (const i in this.validateForm.controls) {
       this.validateForm.controls[i].markAsDirty();
       this.validateForm.controls[i].updateValueAndValidity();
     }
+    console.log(this.validateForm)
   }
   updateConfirmValidator(): void {
     /** wait for refresh value */
@@ -112,7 +48,5 @@ export class FoodDetailComponent implements OnInit {
     return {};
   };
 
-  getCaptcha(e: MouseEvent): void {
-    e.preventDefault();
-  }
+
 }
--- src/app/pages/food/list/food-list.component.ts
+++ src/app/pages/food/list/food-list.component.ts
@@ -1,95 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import {FoodType, Food} from '../food.common';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { FoodType, Food } from "../food.common";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
+import { environment } from 'src/environments/environment';
+interface Response<T> {
+  data: T;
+}
 @Component({
-  selector: 'app-food-list',
-  templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.scss']
+  selector: "app-food-list",
+  templateUrl: "./food-list.component.html",
+  styleUrls: ["./food-list.component.scss"]
 })
 export class FoodListComponent implements OnInit {
   todos$: Observable<Food[]>;
-  constructor(private http: HttpClient) { }
-  foods: Food[] = [
-    {
-      id: 1,
-      name: '锅包肉',
-      imgUrl: '',
-      price: 45,
-      desc: '酸甜可口',
-      type: FoodType.Dongbei
-    },
-    {
-      id: 2,
-      name: '鱼香肉丝',
-      imgUrl: '',
-      price: 32,
-      desc: '没有鱼',
-      type: FoodType.Sichuan
-    },
-    {
-      id: 3,
-      name: '白切鸡',
-      imgUrl: '',
-      price: 52,
-      desc: '没煮熟就吃',
-      type: FoodType.Guangdong
-    }
-  ];
+  constructor(private http: HttpClient) {}
+  foods: Food[] = [];
 
   ngOnInit() {
-
+    this.getFoodList()
+  }
+  insertFood() {
     const headers = new HttpHeaders().set(
-      'Content-type',
-      'application/json; charset=UTF-8'
+      "Content-type",
+      "application/json; charset=UTF-8"
     );
     this.http
-        .post(
-          'http://127.0.0.1:3000/food/createFood',
-          new Food({
-            id: 0,
-            name: '糖醋里脊',
-            type: 1,
-            imgUrl: '1',
-            desc: '啦啦',
-            price: 100.5
-          }),
-          { headers }
-        )
-        .subscribe(
-          val => {
-            console.log('Post call successful value returned in body', val);
-          },
-          error => {
-            console.log('Post call in error', error);
-          },
-          () => {
-            console.log('The Post observable is now completed.');
-          }
-        );
-    }
-    // const params = new HttpParams({ fromObject: { _page: '1', _limit: '10' } });
-
-    // this.todos$ = this.http.get<Food[]>(
-    //   'http://127.0.0.1:3000/food/getFoodList',
-    //   {params},
-    // ).pipe(tap(console.log));
-    // this.todos$.subscribe(res => {
-    //   console.log(res);
-    //   this.foods = res;
-    // });
-
-  //  .subscribe(res => {
-  //     console.log(res);
-  //   });
-    // this.todos$.toPromise().then(val => {
-    //   console.log(val);
-    // });
-    // this.foods.push(new Food())
+      .post(
+        "http://127.0.0.1:3000/food/createFood",
+        new Food({
+          id: 0,
+          name: "糖醋里脊",
+          type: 1,
+          imgUrl: "1",
+          desc: "啦啦",
+          price: 100.5
+        }),
+        { headers }
+      )
+      .subscribe(
+        val => {
+          console.log("Post call successful value returned in body", val);
+        },
+        error => {
+          console.log("Post call in error", error);
+        },
+        () => {
+          console.log("The Post observable is now completed.");
+        }
+      );
   }
-
-
-
-
-
+  getFoodList() {
+    console.log(environment)
+    this.http.get("food/getList").subscribe((res:Response<Food[]>) => {
+    this.foods =res.data
+    });
+  }
+  delFood(id:number){
+    this.http.post("food/delete",{id}).subscribe(res=>{
+      console.log(res)
+      this.getFoodList()
+    })
+  }
+}
